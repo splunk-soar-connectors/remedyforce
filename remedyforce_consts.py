0eccd3eda78c716e91b58505810a13b541f619fa
jgagan <jgagan@splunk.com>
2021-06-30 11:58:33 +0530

    resolving merge conflicts

--- remedyforce_consts.py
+++ remedyforce_consts.py
@@ -1,3 +1,17 @@
+# --
+# File: remedyforce_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2018
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
+
 REMEDY_JSON_USERNAME = "username"
 REMEDY_JSON_PASSWORD = "password"
 REMEDY_JSON_DESCRIPTION = "description"
